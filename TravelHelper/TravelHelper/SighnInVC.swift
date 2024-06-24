0414db8f8675f0a9ad6db8fadf260f504d96390a
Vadim <91436203+VadimWictorovich@users.noreply.github.com>
2024-06-24 22:27:59 +0300

    setup constraints

---
+++ TravelHelper/TravelHelper/SighnInVC.swift
@@ -0,0 +1,113 @@
+//
+//  SignInVC.swift
+//  TravelHelper
+//
+//  Created by Вадим Игнатенко on 9.06.24.
+//
+
+import UIKit
+
+final class SighnInVC: UIViewController {
+    
+    private let welcomeLabel = UILabel()
+    private let loginTF = UITextField()
+    private let passwordTF = UITextField()
+    private let butSignIn = UIButton()
+    private let butSignUp = UIButton()
+    private let faceIDButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.addSubview(welcomeLabel)
+        view.addSubview(loginTF)
+        view.addSubview(passwordTF)
+        view.addSubview(butSignIn)
+        view.addSubview(butSignUp)
+        view.addSubview(faceIDButton)
+
+        
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        welcomeLabel.numberOfLines = 0
+        
+        
+        loginTF.translatesAutoresizingMaskIntoConstraints = false
+        loginTF.layer.cornerRadius = 15
+        
+        passwordTF.translatesAutoresizingMaskIntoConstraints = false
+        passwordTF.layer.cornerRadius = 15
+        
+        butSignIn.translatesAutoresizingMaskIntoConstraints = false
+        butSignIn.layer.cornerRadius = 15
+        butSignIn.layer.borderWidth = 5
+        butSignIn.layer.borderColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        
+        butSignUp.translatesAutoresizingMaskIntoConstraints = false
+        butSignUp.layer.cornerRadius = 15
+        butSignUp.layer.borderWidth = 5
+        butSignUp.layer.borderColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        
+        faceIDButton.translatesAutoresizingMaskIntoConstraints = false
+        faceIDButton.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        
+        setupConstraints()
+        updateViewModel()
+    }
+    
+    private func updateViewModel() {
+        
+    }
+    
+    private func setupConstraints() {
+            // Add constraints for welcomeLabel
+            NSLayoutConstraint.activate([
+                welcomeLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+                welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ])
+            
+            // Add constraints for loginTF
+            NSLayoutConstraint.activate([
+                loginTF.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 20),
+                loginTF.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                loginTF.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                loginTF.heightAnchor.constraint(equalToConstant: 50)
+            ])
+            
+            // Add constraints for passwordTF
+            NSLayoutConstraint.activate([
+                passwordTF.topAnchor.constraint(equalTo: loginTF.bottomAnchor, constant: 20),
+                passwordTF.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                passwordTF.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                passwordTF.heightAnchor.constraint(equalToConstant: 50)
+            ])
+            
+            // Add constraints for butSignIn
+            NSLayoutConstraint.activate([
+                butSignIn.topAnchor.constraint(equalTo: passwordTF.bottomAnchor, constant: 20),
+                butSignIn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                butSignIn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                butSignIn.heightAnchor.constraint(equalToConstant: 50)
+            ])
+            
+            // Add constraints for butSignUp
+            NSLayoutConstraint.activate([
+                butSignUp.topAnchor.constraint(equalTo: butSignIn.bottomAnchor, constant: 20),
+                butSignUp.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                butSignUp.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                butSignUp.heightAnchor.constraint(equalToConstant: 50)
+            ])
+            
+            // Add constraints for faceIDButton
+            NSLayoutConstraint.activate([
+                faceIDButton.topAnchor.constraint(equalTo: butSignUp.bottomAnchor, constant: 20),
+                faceIDButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                faceIDButton.widthAnchor.constraint(equalToConstant: 50),
+                faceIDButton.heightAnchor.constraint(equalToConstant: 50),
+                faceIDButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30)
+            ])
+        }
+  
+}
